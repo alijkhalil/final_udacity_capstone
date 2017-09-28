09fd92d78d01f8337774fd47599452d027a1d977
Jado <alijkhalil@gmail.com>
2017-09-28 04:57:19 -0400

    Made set up script have a specific version for each associated repo so script would never get an incompatible version of those repos.

--- set_up.sh
+++ set_up.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# Set up variables and local directory
 UTIL_REPO_NAME="dl_utilities"
+UTIL_REPO_GIT_HASH="a4c70c3c35794a85c411ffda5bd5809027d310f1"
+
 CNN_MODEL_REPO_NAME="state_of_art_cnns"
+CNN_MODEL_REPO_GIT_HASH="fa32da3967572f37de399241a87bcc9f7a9e028a"
 
 cd `dirname $0`
+
 
 # Get necessary repo's
 echo -n "Getting other repositories needed for the project...  "
@@ -17,6 +22,10 @@ if [ ! -d $UTIL_REPO_NAME ]; then
     fi
 fi
 
+cd $UTIL_REPO_NAME
+git checkout $UTIL_REPO_GIT_HASH > /dev/null 2>&1
+cd - > /dev/null 2>&1
+
 if [ ! -d $CNN_MODEL_REPO_NAME ]; then
     git clone git@github.com:alijkhalil/"$CNN_MODEL_REPO_NAME".git > /dev/null 2>&1
     
@@ -25,6 +34,11 @@ if [ ! -d $CNN_MODEL_REPO_NAME ]; then
         exit 1
     fi
 fi
+
+cd $CNN_MODEL_REPO_NAME
+git checkout $CNN_MODEL_REPO_GIT_HASH > /dev/null 2>&1
+cd - > /dev/null 2>&1
+
 
 # Print success and exit
 echo "Done!"
